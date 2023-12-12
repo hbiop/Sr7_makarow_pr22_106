1d1fffe366de43e95f0010cb1fa7c00694c8351f
Егор Макаров <egor.makarow2004@gmail.com>
2023-12-12 13:09:48 +0700

    4

--- app/src/main/java/com/example/sr7_makarow_pr22_106/MainActivity.java
+++ app/src/main/java/com/example/sr7_makarow_pr22_106/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.window.SplashScreen;
 
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
--- app/src/main/java/com/example/sr7_makarow_pr22_106/SignIn.java
+++ app/src/main/java/com/example/sr7_makarow_pr22_106/SignIn.java
@@ -1,18 +1,62 @@
 package com.example.sr7_makarow_pr22_106;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener, TextWatcher{
     Button next;
+    EditText editText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
         next = findViewById(R.id.next);
         next.setEnabled(false);
+        editText = findViewById(R.id.editTextText);
+        editText.setOnTouchListener(this);
+        editText.addTextChangedListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //editText.getText().clear();
+
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        editText.getText().clear();
+        return false;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if(editText.getText().length() > 0)
+        {
+            next.setEnabled(true);
+        }
+        else
+        {
+            next.setEnabled(false);
+        }
     }
 }
